7fa5e15287f781812e20e5e9dc4b411598f39002
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2014-02-12 00:53:19 +0100

    ARM: Perform the creation of procfs node for VFP later
    The creation of procfs node for VFP bounce reporting failed
    when placed in early init, so perform this creation later.

--- arch/arm/vfp/vfpmodule.c
+++ arch/arm/vfp/vfpmodule.c
@@ -795,9 +795,7 @@ static int __init vfp_init(void)
 {
 	unsigned int vfpsid;
 	unsigned int cpu_arch = cpu_architecture();
-#ifdef CONFIG_PROC_FS
-	static struct proc_dir_entry *procfs_entry;
-#endif
+
 	if (cpu_arch >= CPU_ARCH_ARMv6)
 		on_each_cpu(vfp_enable, NULL, 1);
 
@@ -867,7 +865,19 @@ static int __init vfp_init(void)
 		}
 	}
 
+	return 0;
+}
+
+/*
+ * VFP late initialisation.
+ */
+static int __init vfp_late_init(void)
+{
 #ifdef CONFIG_PROC_FS
+	static struct proc_dir_entry *procfs_entry;
+
+	pr_debug("Create procfs node for VFP bounce reporting\n");
+
 	procfs_entry = create_proc_entry("cpu/vfp_bounce", S_IRUGO, NULL);
 
 	if (procfs_entry)
@@ -880,3 +890,4 @@ static int __init vfp_init(void)
 }
 
 core_initcall(vfp_init);
+late_initcall(vfp_late_init);
