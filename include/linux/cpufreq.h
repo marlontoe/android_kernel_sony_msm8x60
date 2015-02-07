59d399328bfe9b13586346697e7efa1928e67db8
marlontoe <inclusionrec@googlemail.com>
2015-02-07 20:11:25 +0100

    enabled cpu governors & bumped version

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -396,6 +396,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
 extern struct cpufreq_governor cpufreq_gov_adaptive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_adaptive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
 #endif
 
 
